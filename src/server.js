07ac105cb2129c2dce9f07a4450cd01829600692
alexscub <alexscub1@gmail.com>
2019-08-24 12:02:45 +0300

    homework-2

--- src/server.js
+++ src/server.js
@@ -1,26 +1,25 @@
 const http = require('http');
 const url = require('url');
 const router = require('./routes/router');
+// const fs = require('fs');
 
 // const options = {
-//   key: fs.readFileSync('src/rootCA.key'),
-//   cert: fs.readFileSync('src/rootCA.pem')
+//   key: fs.readFileSync('server.key'),
+//   cert: fs.readFileSync('server.crt')
 // }
 
 const startServer = port => {
 
   const server = http.createServer((request, response) => {
-
     const parsedUrl = url.parse(request.url);
     request.parsedPath = parsedUrl.path.split('/')
     const func = router[request.parsedPath[1]] || router.default;
-    console.log(func)
     func(request, response);
   });
 
   server.listen(port, (err) => {
     if (err) {
-      return  err
+      throw  err
     }
     console.log(`server is listening on ${port}`)
   });
